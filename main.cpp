365c8281796cd00bff7baf410eb4d6bfc6cffd0a
Edgar Cuesta <edgarcufe@gmail.com>
2014-06-24 22:40:14 -0500

    Ejercicio 4 de lazyfoo, strectching e imágenes optimizadas en la carga...funcionó de una

--- main.cpp
+++ main.cpp
@@ -14,11 +14,11 @@ enum KeyPressSurfaces
 //Inicia SDL y crea una ventana
 SDL_Window* init();
 //Carga los medios
-bool loadMedia(SDL_Surface** surfaces);
+bool loadMedia(SDL_Surface** surfaces, SDL_Surface* screenSurface);
 //Libera memoria y recursos, además de apagar SDL
 void close(SDL_Window *win);
 //CArga individualmente una imagen
-SDL_Surface* loadSurface(std::string path);
+SDL_Surface* loadSurface(std::string path, SDL_Surface* screenSurface);
 int main(int argc, char** arcv)
 {
     //Surface de la ventana
@@ -37,7 +37,7 @@ int main(int argc, char** arcv)
     {
 	//Saca el surface	
 	screenSurface = SDL_GetWindowSurface(window);
-	bool test = loadMedia(keyPresSurfaces);
+	bool test = loadMedia(keyPresSurfaces, screenSurface);
 	if(!test)
 	{
 	    return 1;
@@ -84,7 +84,13 @@ int main(int argc, char** arcv)
 		    }
 		}
 		//Llena el surface
-		SDL_BlitSurface(currentSurface, NULL, screenSurface, NULL);
+		//Stretching the surface
+		SDL_Rect stretchRect;
+		stretchRect.x = 0;
+		stretchRect.y = 0;
+		stretchRect.w = SCREEN_WIDTH;
+		stretchRect.h = SCREEN_HEIGHT;
+		SDL_BlitSurface(currentSurface, NULL, screenSurface, &stretchRect);
 		//Actualiza surface
 		SDL_UpdateWindowSurface(window);
 	    }
@@ -127,39 +133,39 @@ SDL_Window* init()
     }
 }
 //Se cargan todas las imagenes
-bool loadMedia(SDL_Surface** surfaces)
+bool loadMedia(SDL_Surface** surfaces, SDL_Surface* screenSurface)
 {
     //Se carga surface por defecto
     bool success =  true;
-    surfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("images/press.bmp");
+    surfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("images/press.bmp", screenSurface);
     if(surfaces[KEY_PRESS_SURFACE_DEFAULT] == nullptr)
     {
 	std::cout<<"No se puede cargar la imagen por defecto" <<std::endl;
 	success = false;
     }
     //Cargando images para tecla arriba
-    surfaces[KEY_PRESS_SURFACE_UP] = loadSurface("images/up.bmp");
+    surfaces[KEY_PRESS_SURFACE_UP] = loadSurface("images/up.bmp", screenSurface);
     if(surfaces[KEY_PRESS_SURFACE_UP] == nullptr)
     {
 	std::cout<<"No se puede cargar la imagen para tecla arriba " <<std::endl;
 	success = false;
     }    
     //Cargando imagen para tecla abajo
-    surfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("images/down.bmp");
+    surfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("images/down.bmp", screenSurface);
     if(surfaces[KEY_PRESS_SURFACE_DOWN] == nullptr)
     {
 	std::cout<<"No se puede cargar la imagen para tecla abajo " <<std::endl;
 	success = false;
     }
     //Imagen para la izquierda
-    surfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("images/left.bmp");
+    surfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("images/left.bmp", screenSurface);
     if(surfaces[KEY_PRESS_SURFACE_LEFT] == nullptr)
     {
 	std::cout<<"No se puede cargar la imagen para tecla izquierda " <<std::endl;
 	success = false;
     }
     //CArgando imagen para la derecha
-    surfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("images/right.bmp");
+    surfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("images/right.bmp", screenSurface);
     if(surfaces[KEY_PRESS_SURFACE_RIGHT] == nullptr)
     {
 	std::cout<<"No se puede cargar la imagen por derecha" <<std::endl;
@@ -174,8 +180,11 @@ void close(SDL_Window* win)
     win = nullptr;
     SDL_Quit();
 }
-SDL_Surface* loadSurface(std::string path)
+//Optimizar la carga del surface
+SDL_Surface* loadSurface(std::string path, SDL_Surface* screenSurface)
 {
+    //Surface optimizado
+    SDL_Surface* optimizedSurface = nullptr;
     //Carga el surface del path especificado
     SDL_Surface* loadedSurface = nullptr;
     loadedSurface = SDL_LoadBMP(path.c_str());
@@ -183,5 +192,15 @@ SDL_Surface* loadSurface(std::string path)
     {
 	std::cout<<"Incapaz de abrir la imagen " << path <<" SDL Error " << SDL_GetError() << std::endl;
     }
-    return loadedSurface;
+    else
+    {
+	optimizedSurface = SDL_ConvertSurface(loadedSurface,screenSurface->format, 0);
+	if(optimizedSurface == nullptr)
+	{
+	    std::cout<<"Incapaz de optimizar imagen "<< path <<" SDL Error: " << SDL_GetError()<<std::endl; 
+	}
+	//Liberando surface cargada
+	SDL_FreeSurface(loadedSurface);
+    }
+    return optimizedSurface;
 }
